628975a2b9bbc8426d607d473b66b4627639d8a3
soonchan <soonchan@github.com>
2018-08-28 20:44:03 +0900

    imit

---
+++ Assets/02.Script/Manager_Control.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager_Control : MonoBehaviour {
+
+    public bool game = false;
+
+	void Update () {
+		
+        if(game)
+        {
+            Vector3 tpos = Input.GetTouch(0).position;
+            if(tpos.x <= Screen.width / 2) //왼쪽
+            {
+                // 공격
+                // 드래그 확인
+                if(true)
+                {
+
+                }
+            }
+            else if(tpos.x >= Screen.width / 2) //오른쪽
+            {
+                // 공격
+                // 드래그 확인
+                if (true)
+                {
+
+                }
+            }
+        }
+	}
+}
